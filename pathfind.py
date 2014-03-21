ebb1000a49fe4609b9723f762ae5618023923a16
Erik Potter <epottercg@gmail.com>
2014-03-21 15:45:14 -0500

    Rewrite in OO. Much clearer.

--- pathfind.py
+++ pathfind.py
@@ -4,22 +4,46 @@ import sys
 import ipdb
 from termcolor import colored
 
-grid_pic = """
+small_maze = """
 |||||||||
-|S|     |
-| | | |||
+|S      |
+| ||| |||
 |       |
-| ||||| |
+| || || |
 |       |
-| |||||||
+| || ||||
 |      E|
 |||||||||
 """
 
-sp = (1,1)
-ep = (8,8)
+large_maze = """
+|||||||||||||||||||||||||
+|S     ||     ||||||    |
+| ||||    ||| |||||  || |
+|    | ||||||       ||| |
+| || |   |||| |||  || | |
+|||| |||      ||||    | |
+||   |||| || ||| |||||| |
+|| |      || | |        |
+|| || |||||| | | ||| ||||
+|  || |||||| | | |      |
+|| || ||     | | | ||||||
+|| || || ||||| | |     ||
+|| ||    ||    | | ||| ||
+|| |||| ||| |||| |   | ||
+||   |           ||| | ||
+|| | | ||||||||||| | | ||
+|  | | ||      |   | | ||
+| |||| || |||| | | | ||||
+| |   ||    || | ||| ||||
+| | |||||||||  | ||  ||||
+| | ||        || ||     |
+| |    ||||||||| |||||| |
+| ||||||||||      ||||| |
+|            ||||      E|
+|||||||||||||||||||||||||
+"""
 
-# Construct grid from ascii pic
 def grid_from_pic(pic):
     '''
     returns a 2D matrix (grid representation) from a multiline ascii string
@@ -28,71 +52,48 @@ def grid_from_pic(pic):
     grid = [[cell for cell in row] for row in pic.split('\n')][1:-1]
     return grid
 
-def print_grid(grid):
-    '''
-    prints a pretty representation of the grid to stdout
-    '''
 
-    for row in grid:
-        print(''.join(row))
+class Maze(object):
+    def __init__(self, grid):
+        self.solutions = []
+        self.grid = [[ Cell(self, x, y, char) \
+                for x, char in enumerate(row) ]
+                    for y, row in enumerate(grid) ]
 
-def print_path(grid, path):
-    '''
-    prints a pretty representation of a given path overlaid on a grid to
-    stdout
-    '''
+        for row in self.grid:
+            for cell in row:
+                if cell.char is 'S':
+                    self.starting_point = cell
+                elif cell.char is 'E':
+                    self.ending_point = cell
 
-    for cell in path:
-        if cell:
-            grid[cell[1]][cell[0]] = colored('.', 'red')
+    def print_grid(self, path=None):
+        '''
+        prints a pretty representation of the grid to stdout
+        '''
+        for row in self.grid:
+            print()
+            for cell in row:
+                if path:
+                    if cell in path:
+                        print(colored('.', 'red'), end='')
+                        continue
+                cell.print_char()
+        print()
+        print()
 
-    print()
-    print_grid(grid)
-    print()
+    def cell_at(self, x, y):
+        return self.grid[y][x]
+
+    def solve(self):
+        self.starting_point.pathfind([])
 
 
-def is_open(cell):
-    '''
-    returns True is cell is not a wall
-    '''
-
-    return cell is not '|'
-
-def find_open_cells(grid, cells):
-    '''
-    returns a list of cells that are not walls
-    '''
-
-    return [ c for c in cells if is_open(grid[c[1]][c[0]]) ]
-
-def find_point_from_char(grid, char):
-    for y, row in enumerate(grid):
-        try:
-            x = row.index(char)
-            sp = x, y
-        except ValueError:
-            pass
-    return sp
-
-def cell_from_offset(grid, cell, offset):
-    '''
-    takes a cell as (x, y) tuple and an offset as (x, y) tuple and returns
-    the translated (x, y) tuple if it exists
-    '''
-
-    x = cell[0] + offset[0] 
-    y = cell[1] + offset[1] 
-
-    if x < 0 or y < 0:
-        raise IndexError("Cell (%s, %s) outside of grid bounds." % (x, y))
-
-    return (x, y)
-
-def cells_surrounding(grid, cell):
-    '''
-    returns a list of (x, y) tuples for each cell adjacent up, down, left, and
-    right
-    '''
+class Cell(object):
+    colors = {
+        'S': 'green',
+        'E': 'red',
+    }
 
     directions = [
         (0, -1), #up
@@ -100,83 +101,86 @@ def cells_surrounding(grid, cell):
         (-1, 0), #left
         (1,  0), #right
     ]
-    cells = []
 
-    for d in directions:
+    def __init__(self, maze, x, y, char=''):
+        self.maze = maze
+        self.x = x
+        self.y = y
+        self.char = char
+
+        self.is_wall = self.char is '|'
+
+    def __repr__(self):
+        return "(%s, %s)" % (self.x, self.y)
+
+    def print_char(self):
         try:
-            c = cell_from_offset(grid, cell, d)
-            cells.append(c)
-        except IndexError:
-            pass
+            print(colored(self.char, Cell.colors[self.char]), end='')
+        except KeyError:
+            print(colored(self.char, 'white'), end='')
 
-    return cells
+    def pathfind(self, path):
+        branches = [ n for n in self.neighbors \
+                        if n.is_open \
+                        and not n in path ]
+        #print('Branches:', branches)
 
-def path_from_point(grid, endpoint, path):
-    paths = []
+        for i, b in enumerate(branches):
+            #print('Branch', i, b)
+            if b is self.maze.ending_point:
+                self.maze.solutions.append(path + [self])
+            else:
+                b.pathfind(path + [self])
 
-    last = path[-1]
+    @property
+    def is_open(self):
+        '''
+        returns True is cell is not a wall
+        '''
 
-    #print('last', last)
-    #print('endpoint', endpoint)
-    if last == endpoint:
-        #print('\nyay\n')
-        #print(path)
-        return path
+        return not self.is_wall
 
-    surrounding = cells_surrounding(grid, last)
-    open_cells = find_open_cells(grid, surrounding)
+    @property
+    def neighbors(self):
+        '''
+        returns a list of (x, y) tuples for each cell adjacent up, down, left, and
+        right
+        '''
 
-    if not open_cells:
-        raise Exception("No open cells.")
+        cells = []
 
-    for o in open_cells:
-        if not o in path:  # prevent intersections
-            p = path_from_point( grid, endpoint, path + [o] )
-            if p:
-                paths.append(p)
+        for direction in Cell.directions:
+            x = self.x + direction[0]
+            y = self.y + direction[1]
 
-    if len(paths) == 1:
-        return paths[0]
+            try:
+                cells.append(self.maze.cell_at(x, y))
+            except IndexError:
+                pass
 
-    return paths
+        return cells
 
 
 def run():
     '''
-    main loop.
     consumes an ASCII grid representation and creates a 2D matrix.
     finds the start (sp) and end (ep) points and finds shortest path.
     '''
 
-    grid = grid_from_pic(grid_pic)
-    print_grid(grid)
+    grid = grid_from_pic(large_maze)
+    maze = Maze(grid)
 
-    sp = find_point_from_char(grid, 'S')
-    ep = find_point_from_char(grid, 'E')
+    maze.print_grid()
+
+    maze.solve()
+    solutions = maze.solutions
+    shortest = min(solutions, key=len)
+
+    print('Found %s solutions.' % len(solutions))
     print()
-    print('starting point:', sp)
-    print('ending point:',   ep)
-    print()
-
-    paths = path_from_point(grid, ep, [sp])
-
-    if not paths:
-        raise Exception("No path could be found.")
-
-    if type(paths[0]) is not list:
-        paths = [paths]
-
-    print()
-    print('Found %s paths.' % len(paths))
-    print(paths)
-    for i, path in enumerate(paths):
-        n = i + 1
-        print()
-        print('Path %s' % n)
-        print('\tLength: %s' % len(path))
-        print('\t', path)
-
-        print_path(grid, path)
+    print('Shortest path (%s steps):' % len(shortest))
+    print(shortest)
+    maze.print_grid(path=shortest)
 
 
 if __name__ == "__main__":
